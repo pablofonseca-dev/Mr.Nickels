8e5919a36644621037a5a31d515bab7d247494be
pablofonseca-dev <pablo.fonsecam@outlook.com>
2021-03-31 05:41:16 -0600

    pablofonseca-dev: Command say.js improved, the system was crashing due to data entered by the user

--- commands/say.js
+++ commands/say.js
@@ -5,6 +5,9 @@ module.exports = {
     "name": "say", 
     "description": "The bot should say what the user types", 
     execute(message, args){
+        if(args.length == 0){
+            message.reply(`I need more arguments to develop that operation`);
+        }
         message.channel.send(args.join(' '));
     }
 }
