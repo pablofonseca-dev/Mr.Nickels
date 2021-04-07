7139d1d315f734c00e87cda7504ec31e8fb288bb
pablofonseca-dev <pablo.fonsecam@outlook.com>
2021-04-07 11:34:53 -0600

    pablofonseca-dev: Enviroment prepared to connect Mr.Nickels to its MongoDB Database

---
+++ src/models/exceptions.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+//*Create Exceptions Schema And Model 
+
+const MODEL_NAME = "ExceptionMessages";
+
+const EXCEPTIONS_SCHEMA = new Schema({
+    ExceptionKey: String, 
+    ExceptionDescription: String, 
+}); 
+
+/**
+ * Everytime this coder creates a collection based
+ * on the Exception Messages Schema, store that object in
+ * the model name collection that is available in MongoDB. 
+ * */
+const ExceptionsModel = mongoose.Model(MODEL_NAME, EXCEPTIONS_SCHEMA);
+
+module.exports = ExceptionsModel; 
